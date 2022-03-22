a08afa5df1a8213a381c6c732ab0412ba2789c13
Latifah <l123868@gmail.com>
2022-03-22 20:53:22 +0800

    Initial Commit

---
+++ HW6 dessert picker(page control_segmented control_button_gesture)/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HW6 dessert picker(page control_segmented control_button_gesture)
+//
+//  Created by Latifah on 2022/3/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
